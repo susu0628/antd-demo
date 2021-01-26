23d5552154d5fe78abc9144261158189c25ebabb
jiangsusu <jiangsusu7586@dingtalk.com>
2021-01-26 16:07:18 +0800

    vue双向数据绑定实现

--- src/myVue/myVue.js
+++ src/myVue/myVue.js
@@ -1,9 +1,10 @@
-function observe (data) { // 监听器 利用Object.defineProperty() 来监听每个数据的变动
+// 监听器 利用Object.defineProperty() 来监听每个数据的变动
+function observe (data, vm) {
   if (!data || typeof data !== 'object') {
     return 
   }
   Object.keys(data).map((key) => {
-    defineReactive(data, key, data[key])
+    defineReactive(vm, key, data[key])
   })
 }
 function defineReactive (data, key, value) {
@@ -11,7 +12,6 @@ function defineReactive (data, key, value) {
   const dep = new Dep()
   Object.defineProperty(data, key, {
     get: () => {
-      console.log('target', Dep.target)
       Dep.target && dep.addSub(Dep.target)
       return value
     },
@@ -23,64 +23,93 @@ function defineReactive (data, key, value) {
     }
   })
 }
-// var data = {
-//   name: 'jiang'
-// }
-// observe(data)
-// data.name = 'tan'
-// data.name = 'tlp'
-// console.log(data.name)
 
 // 消息订阅器 -- 维护一个数组，用来收集所有的订阅者
 function Dep () {
   this.subs = []
 }
 Dep.prototype = {
-  addSub: (sub) => {
+  addSub: function (sub) {
     (this.subs || []).push(sub)
   },
-  notify: () => {
-    console.log('subs', this)
+  notify: function () {
     this.subs.forEach(sub => {
       sub.update()
     })
   }
 }
 Dep.target = null
+
+
 // 实现订阅者
-function Watcher (vm, key, cb) {
+function Watcher (vm, node, name) {
+  Dep.target = this // 缓存自己
   this.vm = vm
-  this.key = key
-  this.cb = cb
-  this.value = this.get() 
+  this.node = node
+  this.name = name
+  this.update()
+  Dep.target = null // 释放自己
 }
-Watcher.prototype = {
-  update: () => {
-    this.run()
+Watcher.prototype = { // 原型中的方法，不可更改为箭头函数，不然this的指向是window，而不是当前构造函数
+  update: function () {
+    this.get()
+    this.node.nodeValue = this.value
   },
-  run: () => {
-    var value = this.vm.data[this.key];
-    var oldVal = this.value;
-    if (value !== oldVal) {
-      this.value = value;
-      this.cb.call(this.vm, value, oldVal);
-    }
-  },
-  get: function() {
-    Dep.target = this;  // 缓存自己
-    var value = this.vm.data[this.key]  // 强制执行监听器里的get函数
-    Dep.target = null;  // 释放自己
-    return value;
+  get: function () {
+    this.value = this.vm[this.name]
   }
 }
-function myVue (data, el, key) {
+
+// 解析器 解析Dom节点
+// 对Dom节点的操作频繁，所以将所有的dom节点存入fragment片段里再进行处理
+function nodeToFragment (node, vm) {
+  let fragment = document.createDocumentFragment()
+  let child = node.firstChild
+  while (child) {
+    compile(child, vm)
+    fragment.appendChild(child)
+    child = node.firstChild
+  }
+  return fragment
+}
+// 处理每个节点的函数，如果有Input绑定v-model属性或者有{{XXX}}的文本节点出现，就替换vm实例中data中的内容
+function compile (node, vm) {
+  var reg = /\{\{(.*)\}\}/ // 匹配{{XXX}}中的XXX
+  // 元素节点
+  if (node.nodeType === 1) {
+    let attr = node.attributes
+    // 解析元素节点的所有属性
+    for (let i = 0; i < attr.length; i++) {
+      if (attr[i].nodeName === 'v-model') {
+        let name = attr[i].nodeValue
+        node.addEventListener('input', function (e) {
+          vm[name] = e.target.value
+        })
+        node.value = vm[name]
+        node.removeAttribute('v-model')
+      }
+    }
+  }
+  // 文本节点
+  if (node.nodeType === 3) {
+    console.log('name')
+    if (reg.test(node.nodeValue)) {
+      let name = RegExp.$1
+      name = name.trim()
+      // node.nodeValue = vm[name]
+      new Watcher(vm, node, name) // 不直接赋值，而是绑定一个订阅者
+    }
+  }
+}
+
+
+// vue的构造函数
+function Vue (options) {
+  const {el, data} = options
   this.data = data
-  observe(data)
-  console.log('el', el, key, data[key], data.name)
-  el.innerHTML = this.data.key
-  new Watcher(this, key, (val) => { // name属性 就是这里的订阅器，
-    console.log('val', val)
-    el.innerHTML = val
-  })
+  let id = el
+  observe(data, this)
+  let dom = nodeToFragment(document.getElementById(id), this)
+  document.getElementById(id).appendChild(dom)
   return this
 }
